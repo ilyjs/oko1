69bf19ec2a3247ada99d3917b74c7d5e5f3747d0
Ilya Netkach <iprovod@gmail.com>
2020-01-23 12:38:16 +0300

    add build

---
+++ build/precache-manifest.0b3e2cb6fca6f55dc5f745e6fac6649d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ae657b2a923bbe9a7277fc11a5b177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a629f5589dcff0fa662",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c854462a4cbdf9c8bc91",
+    "url": "/static/js/2.17a49c70.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.17a49c70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a629f5589dcff0fa662",
+    "url": "/static/js/main.7dbeabec.chunk.js"
+  },
+  {
+    "revision": "650a1cc9be78a1d9e9fa",
+    "url": "/static/js/runtime-main.90935db3.js"
+  }
+]);
